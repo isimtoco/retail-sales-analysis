4fba9a977072075d685c0eab45698156f72efd88
isimtoco <isimtoco2@gmail.com>
2026-09-04 16:03:33 -0400

    Created sql file for analysis

---
+++ retail_sales_analysis.sql
@@ -0,0 +1,9 @@
+--Revenue by month
+
+--Top categories by revenue
+
+--Online vs. in-store performance by category
+
+--Repeat vs. new customers
+--Customer concentration: Does the top 20% of customers drive most of the revenue?
+--Discount impact on quantity and order value
